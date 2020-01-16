7f9886a45e3aab7c8c91b22df3a7e657c07a99d7
rojhat15 <47630026+rojhat15@users.noreply.github.com>
2020-01-16 09:23:20 +0100

    materiaal toegevoegd

---
+++ materiaal/aapoverzicht_opdracht_1.php
@@ -0,0 +1,24 @@
+<?php
+ 
+    $apen[] = "Baviaan";
+    $apen[] = "Guereza";
+    $apen[] = "Langoer";
+    $apen[] = "Tamarin";
+    $apen[] = "Neusaap";
+    $apen[] = "Halfaap";
+    $apen[] = "Mandril";
+    $apen[] = "Oeakari";
+    $apen[] = "Faunaap";
+    $apen[] = "Hoelman";
+    $apen[] = "Meerkat";
+    $apen[] = "Oormaki";
+    $apen[] = "Gorilla";
+    $apen[] = "Kuifaap";
+    $apen[] = "Mensaap";
+    $apen[] = "Spinaap";
+?>
+     
+     
+     
+    
+    
