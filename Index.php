afc057da9a9b4ff890d386775217d10d870cd1be
rojhat15 <47630026+rojhat15@users.noreply.github.com>
2020-01-16 09:53:07 +0100

    opdracht 2

--- Index.php
+++ Index.php
@@ -1,1 +1,55 @@
-.
+<?php
+
+//Opdracht 2
+ 
+    $apen[] = "Baviaan";
+    $apen[] = "Guereza";
+    $apen[] = "Langoer";
+    $apen[] = "Tamarin";
+    $apen[] = "Neusaap";
+    $apen[] = "Halfaap";
+    $apen[] = "Mandril";
+    $apen[] = "Oeakari";
+    $apen[] = "Faunaap";
+    $apen[] = "Hoelman";
+    $apen[] = "Meerkat";
+    $apen[] = "Oormaki";
+    $apen[] = "Gorilla";
+    $apen[] = "Kuifaap";
+    $apen[] = "Mensaap";
+    $apen[] = "Spinaap";
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Monkey Business</title>
+		<link href="https://fonts.googleapis.com/css?family=Bangers&display=swap" rel="stylesheet">
+		<style>
+			body{
+				font-family: Arial;
+				text-align: center;
+			} 
+				
+			p{
+				font-family: 'Bangers', cursive;
+				color: #ff8400;
+				font-size: 30px;
+			}
+		</style>
+	</head>
+	
+	<body>
+		<img src="images/monkey-business.jpg" alt="monkey business">
+		
+		<h1>select your monkey!</h1>
+		
+		<img src="images/monkey_swings.png" alt="monkey swings">
+		
+		<?php foreach($apen as $aap) { ?>
+			<p><?= $aap; ?></p>
+		<?php } ?>
+
+	</body>
+</html>
